a337cbb202c18cd0a293edca055ffad528c3677c
Wenyin Wei <wenyin.wei.ww@gmail.com>
2022-08-03 13:38:16 +0800

    + MDSplus python path

--- dig/__init__.py
+++ dig/__init__.py
@@ -2,5 +2,11 @@ from appdata import AppDataPaths; _app_paths = AppDataPaths("mhd")
 from pathlib import Path
 mhd_settings_filepath = Path(_app_paths) / "mhd_settings.ini"
 
+
 import configparser; _conf = configparser.ConfigParser(); _conf.read( mhd_settings_filepath )
+
+import sys
+MDSplus_python_path = _conf.get("mdsplus", "python_path") 
+sys.path.append(MDSplus_python_path) # for Windows users, MDSplus is normally installed at "C:\\Program Files\\MDSplus\\python"
+
 MDSplus_server_IP = _conf.get("mdsplus", "server_ip")
