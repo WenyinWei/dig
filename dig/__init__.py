45e592d46a78c30ec6c9be4eba0eb90724b546b5
Wenyin Wei <wenyin.wei.ww@gmail.com>
2022-08-03 13:53:07 +0800

    debug: _app_paths -> _app_paths.app_data_path

--- dig/__init__.py
+++ dig/__init__.py
@@ -1,6 +1,6 @@
 from appdata import AppDataPaths; _app_paths = AppDataPaths("mhd")
 from pathlib import Path
-mhd_settings_filepath = Path(_app_paths) / "mhd_settings.ini"
+mhd_settings_filepath = Path(_app_paths.app_data_path) / "mhd_settings.ini"
 
 
 import configparser; _conf = configparser.ConfigParser(); _conf.read( mhd_settings_filepath )
